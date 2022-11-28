6e704da593983b9a04efcb6a85606afdba997f0c
Mrkotadiya <nirbhayk7281@gmail.com>
2022-11-28 11:57:46 +0530

    second failan

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-            {/* <Navbar/> */}
-            <Counter/>
+            <Navbar/>
+            {/* <Counter/> */}
     </div>
   );
 }
